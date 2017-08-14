80484c3a4c794e3de73d22713a4306e843f0d425
Cody Hutchens <chutchens91@gmail.com>
2017-08-13 21:28:24 -0400

    Finished reversing a Linked List

--- C++/singly-linked-list.cpp
+++ C++/singly-linked-list.cpp
@@ -9,36 +9,35 @@ class SinglyLinkedList
     SinglyLinkedList() {}
     ~SinglyLinkedList() {}
 
-    Node<T>* getHead() 
+    Node<T> *getHead()
     {
         return head;
     }
 
-    Node<T>* getTail() 
+    Node<T> *getTail()
     {
         return tail;
     }
 
-    void DeleteAll() 
+    void DeleteAll()
     {
         Node<T> *next;
-        while(head) 
+        while (head)
         {
             next = head->getNext();
             delete head;
             head = next;
-            
         }
 
         tail = NULL;
-    } 
+    }
 
     void InsertInFront(Node<T> *element)
     {
         element->setNext(head);
         head = element;
 
-        if(head->getNext() == NULL) 
+        if (head->getNext() == NULL)
         {
             tail = element;
         }
@@ -46,13 +45,13 @@ class SinglyLinkedList
 
     bool DeleteFirst()
     {
-        if (head == NULL) 
+        if (head == NULL)
         {
             return false;
         }
 
         head = head->getNext();
-        if(head == NULL) 
+        if (head == NULL)
         {
             tail = NULL;
         }
@@ -60,24 +59,25 @@ class SinglyLinkedList
         return true;
     }
 
-    bool InsertAfter(Node<T> *element, const T &data) 
+    bool InsertAfter(Node<T> *element, const T &data)
     {
         bool result = false;
         Node<T> *current = head;
 
-        if (!head || !element) 
+        if (!head || !element)
         {
             return result;
         }
 
-        while(current && !result) 
+        while (current && !result)
         {
-            if(current == element) {
+            if (current == element)
+            {
                 Node<T> *node = new Node<T>(data);
                 node->setNext(element->getNext());
                 element->setNext(node);
 
-                if(!node->getNext()) 
+                if (!node->getNext())
                 {
                     tail = node;
                 }
@@ -86,44 +86,47 @@ class SinglyLinkedList
 
             current = current->getNext();
         }
-        
+
         return result;
     }
-    
+
     bool Delete(Node<T> *element)
     {
         bool result = false;
         Node<T> *current = head;
 
-        if(!element) 
+        if (!element)
         {
             return result;
         }
 
-        if(head == element) 
+        if (head == element)
         {
             head = element->getNext();
             delete element;
 
-            if(!head) 
+            if (!head)
             {
                 tail = NULL;
             }
             result = true;
         }
 
-        while(current) 
+        while (current)
         {
-            if(current->getNext() == element) {
+            if (current->getNext() == element)
+            {
                 current->setNext(element->getNext());
                 delete element;
 
-                if(current->getNext() == NULL) 
+                if (current->getNext() == NULL)
                 {
                     tail = current;
                 }
-                result = true; 
-            } else {
+                result = true;
+            }
+            else
+            {
                 current = current->getNext();
             }
         }
@@ -131,27 +134,30 @@ class SinglyLinkedList
         return result;
     }
 
-    Node<T>* reverse() {
-       return this->_reverse(this->head);
+    void reverse()
+    {
+        this->_reverse(this->head);
+
+        //Swap head and tail
+        Node<T> *head = this->head;
+        this->head = this->tail;
+        this->tail = head;
     }
 
   private:
     Node<T> *head;
     Node<T> *tail;
 
-    Node<T>* _reverse(Node<T> *head) {
-        if(!head) return NULL;
+    void _reverse(Node<T> *head)
+    {
+        if (!head)
+            return;
+        if (!head->getNext())
+            return;
 
         _reverse(head->getNext());
 
-        if(head->getNext()) 
-        {
-            Node<T> *tmp = head->getNext()->getNext();
-            head->getNext()->setNext(head);
-            head = head->getNext();
-            head->getNext()->setNext(tmp);
-        }
-
-        return head;
+        head->getNext()->setNext(head);
+        head->setNext(NULL);
     }
 };
